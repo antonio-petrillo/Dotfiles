4ff9b156b7bab55313d119f2ecbf33b158b6de94
Antonio Petrillo <antonio.petrillo4@studenti.unina.it>
2024-10-02 13:30:26 +0200

    adding themes for awesome

--- .config/awesome/main/user.lua
+++ .config/awesome/main/user.lua
@@ -1,11 +1,18 @@
 local gears = require("gears")
 
+local themes = {
+    "default",       -- 1
+    "zenburn",       -- 2
+}
+
+local chosen_theme = themes[1]
+
 local _CONSTANTS = {
   modkey    = "Mod4",
   terminal  = "alacritty",
   browser   = "firefox",
   files     = "nautilus",
-  theme_dir = gears.filesystem.get_themes_dir() .. "default/theme.lua",
+  theme_dir = string.format("%s/.config/awesome/themes/%s/theme.lua", os.getenv("HOME"), chosen_theme),
 }
 
 return _CONSTANTS
